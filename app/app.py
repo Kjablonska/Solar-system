39b7db08e6a85c41d0489c2efdedfcc9cea7a457
Kjablonska <k.jablonska8@gmail.com>
2021-04-22 12:01:15 +0200

    get possition vectors

--- app/app.py
+++ app/app.py
@@ -1,6 +1,10 @@
 from flask import Flask, request
+from astroquery.jplhorizons import Horizons
+from astropy.table import Table
 import pymongo
 import json
+
+# sudo systemctl start mongod
 
 app = Flask(__name__)
 
@@ -10,12 +14,8 @@ def home():
 
 @app.route("/getPlanet")
 def get_planetId_by_name():
-    db = connectToDatabase()
-    planet_collection = db["planets"]
     name = request.args.get('name')
-    print(name)
-    data = planet_collection.find_one({'name': name})
-    return data
+    return get_planet(name)
 
 
 @app.route("/getSatellites")
@@ -29,8 +29,31 @@ def get_planets_satellites():
     return json.dumps(satellites)
 
 
-# Put credentials in some condif file.
+@app.route("/getObjectJPLData")
+def get_JPL_data():
+    name = request.args.get('name')
+    planet = get_planet(name)
+    print(planet["_id"])
+
+    obj = Horizons(id = str(planet["_id"]), location='0', epochs = {"start": "2021-04-01", "stop": "2021-04-10", "step": "1d"}, id_type='majorbody')
+
+    vec = obj.vectors()
+    possitons_data = {}
+    for name in vec.colnames:
+        if name in ['x', 'y', 'z', 'vx', 'vy', 'vz']:
+            possitons_data[name] = vec[name].tolist()
+
+    return possitons_data
+
+
+# Put credentials in some condig file.
 def connectToDatabase():
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
     mydb = myclient["celestialBodies"]
     return mydb
+
+
+def get_planet(name):
+    db = connectToDatabase()
+    planet_collection = db["planets"]
+    return planet_collection.find_one({'name': name})
